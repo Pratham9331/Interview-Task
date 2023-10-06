96eb51074b304626a3c411b6643088a2b214bcbb
Prathamesh <phadtareprathamesh@gmailcom>
2023-10-06 15:45:23 +0530

    WonderBizz,Thane

---
+++ Reverse_Character_Excepting_First_Letter/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        string input = "This is a Demo";
+        string[] inp = input.Split(' ');
+
+        string output = reverse(inp);
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+
+    static string reverse(string[] input)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            char[] f = input[i].ToCharArray();
+            Array.Reverse(f, 1, f.Length - 1);
+            input[i] = new string(f);
+        }
+        return string.Join(" ", input);
+    }
+}
